6b74c96724a53b2f01078ac395a03da8e6c2b5bd
jagadeesh R <jdeesh735@gmail.com>
2023-09-28 02:34:37 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "external_ip" {
-  description = "External IP address of the Load Balancer"
-  value       = google_compute_global_forwarding_rule.zi-forwarding-rule.ip_address
-}
+#output "external_ip" {
+#  description = "External IP address of the Load Balancer"
+#  value       = google_compute_global_forwarding_rule.zi-forwarding-rule.ip_address
+#}
