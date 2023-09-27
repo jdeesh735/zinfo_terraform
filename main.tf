062e7e6d17c52fa5f14a987a0dc4aad6cbcdcf0b
jagadeesh R <jdeesh735@gmail.com>
2023-09-28 02:25:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,127 +7,71 @@ terraform {
   }
 }
 
+# Define your provider (Google Cloud).
 provider "google" {
-  credentials = file("kubernetes/zinfo.json")
-  project     = "<your-project-id>"
-  region      = "<your-region>"
+  credentials = file("path/to/your/credentials.json")
+  project     = "zoominfo-project"
+  region      = "us-central1"
 }
 
-resource "google_container_cluster" "zi-cluster" {
+# Create a GKE cluster.
+resource "google_container_cluster" "zi_cluster" {
   name     = "zi-cluster"
   location = "us-central1"
-  initial_node_count = 1
 
-  node_pool {
-    name       = "default-pool"
-    #machine_type = "n1-standard-1"
-    # Other node pool configurations go here
-  }
+  # Specify additional configuration for your cluster (e.g., node pools).
+  # ...
+
+  depends_on = [google_project_service.k8s]
 }
 
-
-resource "google_compute_instance" "zi-instance" {
-  name         = "zi-instance"
-  machine_type = "n1-standard-1"
-  zone         = "<your-zone>"
-  tags         = ["http-server"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    network = "default"
-  }
-
-  depends_on = [google_container_cluster.zi-cluster]
+# Enable the Kubernetes Engine API.
+resource "google_project_service" "k8s" {
+  project = "zoominfo-project"
+  service = "container.googleapis.com"
 }
 
-resource "google_compute_firewall" "zi-firewall" {
-  name    = "zi-firewall"
-  network = "default"
+# Create a GCS Bucket IAM binding.
+resource "google_storage_bucket_iam_binding" "bucket_iam" {
+  bucket = "zinfo-gcs-bucket"
 
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
+  role   = "roles/storage.objectViewer"
+  members = ["serviceAccount:zinfo-sa@zoominfo-project.iam.gserviceaccount.com"]
 }
 
-resource "google_compute_backend_service" "zi-backend-service" {
-  name             = "zi-backend-service"
-  backend {
-    group = google_compute_instance.zi-instance.self_link
-  }
-  #port             = 80
-  protocol         = "HTTP"
-  timeout_sec      = 10
-  enable_cdn       = false
-  health_checks   = [google_compute_http_health_check.zi-health-check.self_link]
-  load_balancing_scheme = "EXTERNAL"
-}
-
-resource "google_compute_http_health_check" "zi-health-check" {
-  name               = "zi-health-check"
+# Create a Load Balancer.
+resource "google_compute_http_health_check" "health_check" {
+  name               = "health-check"
   request_path       = "/"
   port               = 80
   check_interval_sec = 5
-  timeout_sec        = 5
 }
 
-resource "google_compute_url_map" "zi-url-map" {
-  name     = "zi-url-map"
-  
-  default_route_action {
-    cors_policy = google_compute_url_map_default_route_action_cors_policy.zi-cors-policy.self_link
-    timeout_ms = 60000
-    url_redirect_action {
-      https_redirect_action {
-        https_redirect_response_code = "MOVED_PERMANENTLY"
-      }
-    }
-  }
-  
-  default_service = google_compute_backend_service.zi-backend-service.self_link
+resource "google_compute_instance_group" "instance_group" {
+  name        = "instance-group"
+  description = "Instance group for your service"
 
-  route {
-    description = "Route to backend service"
-    match {
-      path = "/"
-    }
-    route_action {
-      weighted_action {
-        default_route_action = google_compute_url_map_default_route_action.zi-default-route-action.self_link
-      }
-    }
+  named_port {
+    name = "http"
+    port = 80
   }
 }
 
+resource "google_compute_backend_service" "backend_service" {
+  name        = "backend-service"
+  description = "Backend service for your application"
+  protocol    = "HTTP"
+  timeout_sec = 300
 
-resource "google_compute_global_forwarding_rule" "zi-forwarding-rule" {
-  name       = "zi-forwarding-rule"
-  target     = google_compute_target_http_proxy.zi-proxy.self_link
+  backend {
+    group = google_compute_instance_group.instance_group.self_link
+  }
+
+  health_checks = [google_compute_http_health_check.health_check.id]
+}
+
+resource "google_compute_global_forwarding_rule" "forwarding_rule" {
+  name       = "forwarding-rule"
+  target     = google_compute_backend_service.backend_service.self_link
   port_range = "80"
-  global    = true
-}
-
-resource "google_compute_target_http_proxy" "zi-proxy" {
-  name = "zi-proxy"
-  url_map = google_compute_url_map.zi-url-map.self_link
-}
-
-resource "google_service_account" "zi-service-account" {
-  account_id   = "zi-service-account"
-  display_name = "My Service Account"
-}
-
-resource "google_storage_bucket_iam_binding" "zi-iam" {
-  bucket = "gs://<your-bucket-name>"
-  role   = "roles/storage.objectViewer"
-  members = [
-    "serviceAccount:${google_service_account.zi-service-account.email}",
-  ]
 }
