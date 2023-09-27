35d78042cbdcab3aa9055d72d0b407306135d14d
jagadeesh R <jdeesh735@gmail.com>
2023-09-28 01:07:10 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "project_id" {
+  description = "Google Cloud Project ID"
+}
+
+variable "region" {
+  description = "Google Cloud Region"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "Google Cloud Zone for GKE"
+  default     = "us-central1-a"
+}
+
+variable "bucket_name" {
+  description = "Name of the GCS Bucket"
+  default     = "my-sample-bucket"
+}
+
+variable "service_account_key" {
+  description = "Path to the service account key JSON file"
+}
