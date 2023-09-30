c8a6d3422139333e52fa13aaf1b3b1097ad40dd7
jagadeesh R <jdeesh735@gmail.com>
2023-09-30 15:42:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,66 +3,74 @@
 # Configure the Google Cloud provider
 provider "google" {
   credentials = file("kubernetes/zinfo.json")
-  project     = "your-project-id"
-  region      = "us-central1" # Replace with your desired region
 }
 
-# Create a GKE cluster
-resource "google_container_cluster" "my_cluster" {
-  name     = "my-gke-cluster"
-  location = "us-central1" # Replace with your desired zone
+resource "google_container_cluster" "my-cluster" {
+  name     = "my-cluster"
+  location = "us-central1-a"
   initial_node_count = 1
-  node_locations    = ["us-central1-a"] # Replace with your desired zone
-}
 
-# Create a GKE Kubernetes deployment and service (deploy your Java/Python application)
-resource "kubernetes_deployment" "my_app" {
-  metadata {
-    name = "my-app"
-  }
+  master_auth {
+    username = ""
+    password = ""
 
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        app = "my-app"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "my-app"
-        }
-      }
-
-      spec {
-        container {
-          image = "your-docker-image" # Replace with your application's Docker image
-          name  = "my-app-container"
-        }
-      }
+    client_certificate_config {
+      issue_client_certificate = false
     }
   }
 }
 
-# Create a Kubernetes LoadBalancer service
-resource "kubernetes_service" "my_service" {
-  metadata {
-    name = "my-service"
+resource "google_container_node_pool" "my-cluster-nodes" {
+  name       = "my-cluster-nodes"
+  location   = google_container_cluster.my-cluster.location
+  cluster    = google_container_cluster.my-cluster.name
+  node_count = 1
+
+  node_config {
+    preemptible  = false
+    machine_type = "n1-standard-1"
+  }
+}
+
+resource "google_compute_address" "lb-ip" {
+  name   = "lb-ip"
+  region = "us-central1"
+}
+
+resource "google_compute_backend_bucket" "bucket" {
+  name          = "bucket-backend"
+  bucket_name   = "your-bucket-name"
+  enable_cdn    = true
+}
+
+resource "google_compute_url_map" "url-map" {
+  name        = "url-map"
+  default_route_action {
+    redirect_action {
+      https_redirect_action {
+        https_redirect = true
+        strip_query    = true
+      }
+    }
   }
 
-  spec {
-    selector = {
-      app = "my-app"
-    }
+  default_service = google_compute_backend_bucket.bucket.id
+}
 
-    port {
-      port        = 80
-      target_port = 8080 # Replace with your application's port
-    }
+resource "google_compute_target_http_proxy" "http-proxy" {
+  name    = "http-proxy"
+  url_map = google_compute_url_map.url-map.id
+}
 
-    type = "LoadBalancer"
-  }
+resource "google_compute_global_forwarding_rule" "forwarding-rule" {
+  name       = "forwarding-rule"
+  target     = google_compute_target_http_proxy.http-proxy.id
+  port_range = "80"
+  ip_address = google_compute_address.lb-ip.address
+}
+
+resource "google_project_iam_member" "bucket-iam-policy" {
+  project = var.project_id
+  role    = "roles/storage.objectViewer"
+  member  = "serviceAccount:service-${google_container_cluster.my-cluster.name}@your-project-id.iam.gserviceaccount.com"
 }
