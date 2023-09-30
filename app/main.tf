e9624cba6556062189061a4cf66065552373c7ac
jagadeesh R <jdeesh735@gmail.com>
2023-10-01 02:33:00 +0530

    Update main.tf

--- app/main.tf
+++ app/main.tf
@@ -34,9 +34,9 @@ resource "google_compute_forwarding_rule" "lb_forwarding_rule" {
   port_range = "80"
 }
 
-resource "google_project_iam_member" "gke_service_account" {
-  project = "zoominfo-project"  # Replace with your actual project ID
-  role    = "roles/storage.admin"  # Replace with the desired IAM role
-  member  = "serviceAccount:zinfo-svc-sa@zoominfo-project.iam.gserviceaccount.com"  # Replace with the service account email
-}
+#resource "google_project_iam_member" "gke_service_account" {
+ # project = "zoominfo-project"  # Replace with your actual project ID
+ # role    = "roles/storage.admin"  # Replace with the desired IAM role
+ # member  = "serviceAccount:zinfo-svc-sa@zoominfo-project.iam.gserviceaccount.com"  # Replace with the service account email
+#}
 
