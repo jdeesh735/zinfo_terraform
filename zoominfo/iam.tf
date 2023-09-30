f41fcef8ce6e5ec1016baccb3a8bcb3f5425e981
jagadeesh R <jdeesh735@gmail.com>
2023-09-30 17:47:24 +0530

    Create iam.tf

---
+++ zoominfo/iam.tf
@@ -0,0 +1,5 @@
+resource "google_project_iam_member" "gcs_access" {
+  project = "zoominfo-project"
+  role    = "roles/storage.objectViewer"
+  member  = "serviceAccount:${google_container_cluster.example.node_config[0].service_account}"
+}
