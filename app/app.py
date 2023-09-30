7868c55dfe300b8679f6b351d2d03e0e0eba2ff3
jagadeesh R <jdeesh735@gmail.com>
2023-09-30 22:01:47 +0530

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def serve_html():
+    # Fetch the HTML file from GCS and serve it here
+    # You'll need to use a library like google-cloud-storage to access GCS.
+    # For simplicity, we'll just return a sample HTML response.
+    return "<html><body><h1>Hello, GKE-GCS Service!</h1></body></html>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
