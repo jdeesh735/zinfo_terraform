772fa6f24d03ebce945812215e1097e1ddb24d86
jagadeesh R <jdeesh735@gmail.com>
2023-09-30 21:01:45 +0530

    Update iam.tf

--- zinfo/iam.tf
+++ zinfo/iam.tf
@@ -1,5 +1,5 @@
 resource "google_project_iam_member" "gke_bucket_access" {
   project = "zoominfo-project"
   role    = "roles/storage.objectViewer"
-  member  = "serviceAccount:${google_container_cluster.zi_cluster.node_service_account}"
+  member  = "serviceAccount:${google_container_cluster.zi_cluster.email}"
 }
