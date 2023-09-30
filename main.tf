dc11493a98217a4e0e5d30739562b883f27294f5
jagadeesh R <jdeesh735@gmail.com>
2023-09-30 16:27:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,76 +1,83 @@
-# main.tf
-
-# Configure the Google Cloud provider
 provider "google" {
-  credentials = file("kubernetes/zinfo.json")
-}
-
-resource "google_container_cluster" "my-cluster" {
-  name     = "my-cluster"
-  location = "us-central1-a"
-  initial_node_count = 1
-
-  master_auth {
-    username = ""
-    password = ""
-
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
-}
-
-resource "google_container_node_pool" "my-cluster-nodes" {
-  name       = "my-cluster-nodes"
-  location   = google_container_cluster.my-cluster.location
-  cluster    = google_container_cluster.my-cluster.name
-  node_count = 1
-
-  node_config {
-    preemptible  = false
-    machine_type = "n1-standard-1"
-  }
-}
-
-resource "google_compute_address" "lb-ip" {
-  name   = "lb-ip"
-  region = "us-central1"
-}
-
-resource "google_compute_backend_bucket" "bucket" {
-  name          = "bucket-backend"
-  bucket_name   = "your-bucket-name"
-  enable_cdn    = true
-}
-
-resource "google_compute_url_map" "url-map" {
-  name        = "url-map"
-  default_route_action {
-    redirect_action {
-      https_redirect_action {
-        https_redirect = true
-        strip_query    = true
-      }
-    }
-  }
-
-  default_service = google_compute_backend_bucket.bucket.id
-}
-
-resource "google_compute_target_http_proxy" "http-proxy" {
-  name    = "http-proxy"
-  url_map = google_compute_url_map.url-map.id
-}
-
-resource "google_compute_global_forwarding_rule" "forwarding-rule" {
-  name       = "forwarding-rule"
-  target     = google_compute_target_http_proxy.http-proxy.id
-  port_range = "80"
-  ip_address = google_compute_address.lb-ip.address
-}
-
-resource "google_project_iam_member" "bucket-iam-policy" {
   project = var.project_id
-  role    = "roles/storage.objectViewer"
-  member  = "serviceAccount:service-${google_container_cluster.my-cluster.name}@your-project-id.iam.gserviceaccount.com"
+  region = var.region
+}
+
+resource "google_kubernetes_engine_cluster" "default" {
+  name = "my-cluster"
+  location = var.region
+  node_count = 3
+}
+
+resource "google_cloud_load_balancing_backend_service" "default" {
+  name = "my-backend-service"
+  port_name = "http"
+  load_balancing_scheme = "EXTERNAL"
+}
+
+resource "google_cloud_load_balancing_forwarding_rule" "default" {
+  name = "my-forwarding-rule"
+  load_balancer_scheme = "EXTERNAL"
+  port_range = "80"
+  target = google_cloud_load_balancing_backend_service.default.self_link
+}
+
+resource "google_cloud_storage_bucket" "default" {
+  name = "my-bucket"
+}
+
+resource "google_cloud_storage_bucket_iam_binding" "default" {
+  bucket = google_cloud_storage_bucket.default.name
+  role = "roles/storage.objectViewer"
+  members = ["serviceAccount:${google_kubernetes_engine_cluster.default.service_account}"]
+}
+
+resource "google_cloud_storage_bucket_iam_binding" "default" {
+  bucket = google_cloud_storage_bucket.default.name
+  role = "roles/storage.objectViewer"
+  members = ["serviceAccount:${google_app_engine_default_version.default.service_account_email}"]
+}
+
+# Python script to fetch a file from the GCS bucket and serve it to clients
+
+import os
+
+from google.cloud import storage
+
+def fetch_file(filename):
+  """Fetches a file from the GCS bucket."""
+
+  client = storage.Client()
+  bucket = client.get_bucket(var.bucket_name)
+  blob = bucket.get_blob(filename)
+  return blob.download_as_string()
+
+def serve_file(filename):
+  """Serves a file to clients."""
+
+  response = make_response(fetch_file(filename), mimetype="text/plain")
+  return response
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  """Returns the index page."""
+
+  return serve_file("index.txt")
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=8080)
+
+# Deploy the Python script to App Engine
+
+resource "google_app_engine_default_version" "default" {
+  runtime = "python39"
+  project = var.project_id
+  app_yaml_file = "app.yaml"
+  source = {
+    files = [
+      ".",
+    ]
+  }
 }
