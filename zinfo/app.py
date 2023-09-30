9f2fcbd713ecbf99300a3cb69ca18f9811a7e5f9
jagadeesh R <jdeesh735@gmail.com>
2023-09-30 20:17:00 +0530

    Update app.py

--- zinfo/app.py
+++ zinfo/app.py
@@ -1,16 +1,21 @@
+from flask import Flask, Response
 from google.cloud import storage
+
+app = Flask(__name__)
 
 def fetch_file_from_gcs(bucket_name, file_name):
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(file_name)
-
-    # You can either return the content or serve it using a web framework like Flask.
     content = blob.download_as_text()
     return content
 
-if __name__ == "__main__":
+@app.route("/")
+def serve_file():
     bucket_name = "zinfo-bucket"
     file_name = "sample.txt"
     file_content = fetch_file_from_gcs(bucket_name, file_name)
-    print(file_content)
+    return Response(file_content, mimetype="text/plain")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
