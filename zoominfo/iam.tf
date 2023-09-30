28a13d22ec9916cd090a8f7cf7558f2351578445
jagadeesh R <jdeesh735@gmail.com>
2023-09-30 17:48:13 +0530

    Update iam.tf

--- zoominfo/iam.tf
+++ zoominfo/iam.tf
@@ -1,5 +1,5 @@
 resource "google_project_iam_member" "gcs_access" {
   project = "zoominfo-project"
   role    = "roles/storage.objectViewer"
-  member  = "serviceAccount:${google_container_cluster.example.node_config[0].service_account}"
+  member  = "serviceAccount:${google_container_cluster.zinfo-cluster.node_config[0].service_account}"
 }
