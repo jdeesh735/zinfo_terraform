5e9f0451befda7369905030949f05745538c9440
jagadeesh R <jdeesh735@gmail.com>
2023-09-30 17:23:29 +0530

    Update lb.tf

--- zinfo/lb.tf
+++ zinfo/lb.tf
@@ -7,7 +7,7 @@ resource "google_compute_global_forwarding_rule" "lb" {
 resource "google_compute_backend_service" "backend_service" {
   name = "my-backend-service"
 
-  health_checks = ["<health_check_name>"]  # Replace with the actual health check name
+  health_checks = ["health_check"]  # Replace with the actual health check name
 
   backend {
     group = google_container_cluster.my_cluster.node_pool[0].instance_group_urls[0]
@@ -17,10 +17,24 @@ resource "google_compute_backend_service" "backend_service" {
 }
 
 resource "google_compute_url_map" "url_map" {
-  name = "my-url-map"
+  name        = "my-url-map"
+  default_service = google_compute_backend_service.backend_service.id
 
   default_route_action {
-    forward_to = google_compute_backend_service.backend_service.self_link
+    cors_policy {
+      max_age        = 3600
+      allow_origins  = ["*"]
+      allow_methods  = ["GET", "POST", "OPTIONS"]
+      allow_headers  = ["*"]
+      expose_headers = ["*"]
+    }
+    timeout = "5s"
+  }
+
+  default_route_action {
+    weighted_action {
+      default_service = google_compute_backend_service.backend_service.id
+    }
   }
 }
 
