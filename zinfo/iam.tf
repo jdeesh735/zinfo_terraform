b9c114c7a564cb7fc5dc599fb02d8f538eb88b69
jagadeesh R <jdeesh735@gmail.com>
2023-09-30 17:18:07 +0530

    Update iam.tf

--- zinfo/iam.tf
+++ zinfo/iam.tf
@@ -22,15 +22,15 @@ resource "google_storage_bucket_iam_binding" "bucket_iam_binding" {
   ]
 }
 
-resource "google_service_account_key" "service_account_key" {
-  account_id   = "my-service-account"
-  service_account_id = google_service_account.my_service_account.email
-  key_algorithm = "RSA"
-}
-
 resource "google_service_account" "my_service_account" {
   account_id   = "my-service-account"
   display_name = "My Service Account"
 }
+
+resource "google_service_account_key" "service_account_key" {
+  service_account_id = google_service_account.service_account.name
+  output_type       = "JSON"
+}
+
 
 # Configure your application to use this service account key for GCS access
