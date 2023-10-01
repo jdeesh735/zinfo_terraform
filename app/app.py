242208134ecfe4d98690cc2d2e9e4e402f9605fd
jagadeesh R <jdeesh735@gmail.com>
2023-10-01 12:56:29 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,14 +1,37 @@
-from flask import Flask
+import os
+from flask import Flask, send_file
+from google.cloud import storage
 
 app = Flask(__name__)
 
+# Set your GCS bucket name and file name
+GCS_BUCKET_NAME = "zinfo-gcs-bucket"
+FILE_NAME = "sample.txt"
+
 @app.route('/')
-def serve_html():
-    
-    # Fetch the HTML file from GCS and serve it here
-    # You'll need to use a library like google-cloud-storage to access GCS.
-    # For simplicity, we'll just return a sample HTML response.
-    return "<html><body><h1>Hello, GKE-GCS Service!</h1></body></html>"
+def home():
+    return "Hello, GKE service!"
+
+@app.route('/fetch/<file_name>')
+def fetch_file(file_name):
+    try:
+        # Initialize GCS client
+        client = storage.Client()
+
+        # Fetch the file from GCS
+        bucket = client.get_bucket(GCS_BUCKET_NAME)
+        blob = bucket.blob(file_name)
+
+        # Serve the file to clients
+        return send_file(
+            blob.download_as_text(),
+            mimetype='text/plain',
+            as_attachment=True,
+            attachment_filename=file_name
+        )
+
+    except Exception as e:
+        return f"Error: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
