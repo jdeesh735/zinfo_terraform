ed17aa2d12745611cbdd088048df6e64923244dc
jagadeesh R <jdeesh735@gmail.com>
2023-10-01 10:44:25 +0530

    Create main.tf

---
+++ info/main.tf
@@ -0,0 +1,11 @@
+provider "google" {
+  credentials = file("../app/zinfo.json")
+  project     = "your-project-id"
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name     = "my-unique-bucket-name"
+  location = "us-central1"  # Choose your preferred location
+
+  uniform_bucket_level_access = true
+}
