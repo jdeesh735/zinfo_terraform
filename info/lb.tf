5393b193d22e39e46656d35e6624005b737eb41a
jagadeesh R <jdeesh735@gmail.com>
2023-10-01 11:59:14 +0530

    Update lb.tf

--- info/lb.tf
+++ info/lb.tf
@@ -1,6 +1,6 @@
 resource "google_compute_global_forwarding_rule" "lb" {
   name        = "my-lb"
-  target      = google_container_cluster.zi_cluster.endpoint
+  target      = google_container_cluster.my_cluster.endpoint
   port_range  = "80"
   # Customize the forwarding rule as needed.
 }
